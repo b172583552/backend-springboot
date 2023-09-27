cfd8b7a27ec322229c602d06c237ff01a838af2f
tom <b172583552@gmail.com>
2023-09-27 15:40:19 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.demo.student;
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/students")
+@CrossOrigin("*")
+@AllArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+
+       return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path="{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+}
